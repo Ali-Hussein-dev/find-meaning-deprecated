9bafe58ec9aa22323546df64e3b2b5bb4a41d718
Ali <spreskill.app@gmail.com>
2020-08-15 19:09:22 +0200

    first commit added dependencies

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.scss";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { BrowserRouter as Router } from "react-router-dom";
+import { Provider, defaultTheme } from "@adobe/react-spectrum";
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider theme={defaultTheme}>
+      <Router>
+        <App />
+      </Router>
+    </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
