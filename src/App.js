cd1311e18851d8d06d7294b1acb4a4ed22499bd0
Ali <spreskill.app@gmail.com>
2020-08-21 19:47:20 +0200

    rendered components in App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.scss";
+import "antd/dist/antd.css";
+// import HomeBtn from "@spectrum-icons/workflow/Home";
+import Home from "./components/Home/Home";
+// import { Link } from "react-router-dom";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { faHome } from "@fortawesome/free-solid-svg-icons";
+import { Flex } from "@adobe/react-spectrum";
+import { Route, Switch } from "react-router-dom";
+import Searching from "./components/Searching/Searching";
+import LocalFooter from "./components/LocalFooter/LocalFooter";
+import NotFound404 from "./components/NotFound404/NotFound404";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Flex direction="column" minHeight="100vh" UNSAFE_className="view">
+     
+      <Flex
+        direction="column"
+        flexGrow="1"
+        justifyContent="center"
+        alignItems="center"
+        minHeight="100vh"
+      >
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/searching">
+            <Searching />
+          </Route>
+          <Route component={NotFound404} />
+        </Switch>
+      </Flex>
+      <LocalFooter />
+    </Flex>
   );
 }
 
