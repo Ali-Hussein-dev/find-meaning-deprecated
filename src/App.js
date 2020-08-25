6772dd9ad57b59fc944bbd304ab25607e8e8a64a
Ali <spreskill.app@gmail.com>
2020-08-25 17:49:46 +0200

    added grid, UI footer issue fixed

--- src/App.js
+++ src/App.js
@@ -6,24 +6,17 @@ import Home from "./components/Home/Home";
 // import { Link } from "react-router-dom";
 // import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 // import { faHome } from "@fortawesome/free-solid-svg-icons";
-import { Flex } from "@adobe/react-spectrum";
+import { View } from "@adobe/react-spectrum";
 import { Route, Switch } from "react-router-dom";
 import Searching from "./components/Searching/Searching";
 import LocalFooter from "./components/LocalFooter/LocalFooter";
 import NotFound404 from "./components/NotFound404/NotFound404";
 
-
 function App() {
   return (
-    <Flex direction="column" minHeight="100vh" UNSAFE_className="view">
-     
-      <Flex
-        direction="column"
-        flexGrow="1"
-        justifyContent="center"
-        alignItems="center"
-        minHeight="100vh"
-      >
+    // ------- grid container / main Adobe Grid container is buggy
+    <main UNSAFE_className="view">
+      <View gridArea="content" alignSelf="stretch">
         <Switch>
           <Route exact path="/">
             <Home />
@@ -33,9 +26,10 @@ function App() {
           </Route>
           <Route component={NotFound404} />
         </Switch>
-      </Flex>
+      </View>
+      {/* -------grid area: footer */}
       <LocalFooter />
-    </Flex>
+    </main>
   );
 }
 
