49b980d09c51a0d87a7e7ec160db2d6398028238
Ali <spreskill.app@gmail.com>
2020-08-25 18:05:05 +0200

    set localfooter in gridArea

--- src/components/LocalFooter/LocalFooter.jsx
+++ src/components/LocalFooter/LocalFooter.jsx
@@ -4,27 +4,28 @@ const LocalFooter = () => {
   const date = new Date();
 
   return (
-    <Footer width="100%" bottom="0" alignSelf="flex-end">
-      <View backgroundColor="gray-50" width="100%" padding="0.5rem">
-        <Flex direction="column" justifyContent="center" alignItems="center">
-          Copyright&copy; {date.getFullYear()}
-          <View>
-            Made with
-            <span role="img" aria-label="">
-              💓
-            </span>
-            by
-            <a
-              href="https://twitter.com/AliHussein_20"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Ali Hussein
-            </a>
-          </View>
-        </Flex>
-      </View>
-    </Footer>
+    // -------grid area: footer
+    // <Footer>
+    <View gridArea="footer" backgroundColor="gray-200" padding="0.5rem">
+      <Flex direction="column" justifyContent="center" alignItems="center">
+        Copyright&copy; {date.getFullYear()}
+        <View>
+          Made with
+          <span role="img" aria-label="">
+            💓
+          </span>
+          by
+          <a
+            href="https://twitter.com/AliHussein_20"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Ali Hussein
+          </a>
+        </View>
+      </Flex>
+      {/* </Footer> */}
+    </View>
   );
 };
 
